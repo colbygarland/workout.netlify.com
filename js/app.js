41f81422cc97f4a3f73096ec532f8503f86b67f8
Colby Garland <colbygarland@gmail.com>
2019-01-03 14:44:35 -0700

    service worker

--- js/app.js
+++ js/app.js
@@ -155,10 +155,10 @@
   });
 
 
-  /*if ('serviceWorker' in navigator) {
+  if ('serviceWorker' in navigator) {
     navigator.serviceWorker
              .register('../service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
-  }*/
+  }
 
 })();
